86ecec98f54ff0a9932e29e67ae92c676a0a3b2b
unknown <jisnavarghese99@gmail.com>
2024-08-08 20:59:15 +0530

    signin completed

--- app.js
+++ app.js
@@ -11,6 +11,43 @@ app.use(Cors())
 
 Mongoose.connect("mongodb+srv://jisna19:jisna9947@cluster0.fzxusrj.mongodb.net/blogAppDb?retryWrites=true&w=majority&appName=Cluster0")
 
+//sign in
+app.post("/signIn",async(req,res)=>{
+    
+    let input=req.body
+    let result=userModel.find({email:req.body.email}).then(
+    (items)=>{
+        if (items.length>0) {
+
+            const passwordValidation=Bcrypt.compareSync(req.body.password,items[0].password)
+            if (passwordValidation) {
+
+                jwt.sign({email:req.body.email},"blogApp",{exporesIn:"1d"},
+                    (error,token)=>{
+                        if (error) {
+                            res.json({"status":"error","errorMessage":error})
+                        } else {
+                            res.json({"status":"success","token":token,"userId":items[0]._id})
+                    }
+            })
+                
+            } else {
+
+                res.json({"status":"Incorrect Password"})
+                
+            }
+            
+        } else {
+            res.json({"status":"Invalid email id"})
+        }
+    }
+    ).catch()
+
+
+})
+
+
+//sign up
 app.post("/signup", async (req, res) => {
     let input = req.body
     let hashedPassword = Bcrypt.hashSync(req.body.password, 10)
